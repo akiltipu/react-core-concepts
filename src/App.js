95980b5b968ef426e4a06bef0cc468e344abcf89
akiltipu <akilmahmod.tipu@gmail.com>
2020-03-11 05:04:33 +0600

    Learned to Pass object to components and access object

--- src/App.js
+++ src/App.js
@@ -4,10 +4,21 @@ import './App.css';
 
 function App() {
   const players = ['Shakib', 'Mustafiz', 'Mashrafe', 'Muminul'];
+
+  const adobeProducts = [
+    {name: 'Photoshop', 
+    price: '$100'},
+    {name: 'Lightroom', 
+    price: '$150'}
+  ]
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>This is React header</h1>
+        <Products product={adobeProducts[0]}></Products>
+        <Products product={adobeProducts[1]}></Products>
+        
         <Player name={players[0]} role="All-rounder"></Player>
         <Player name={players[1]} role="Bowler"></Player>
         <Player name={players[2]} role="Captain"></Player>
@@ -16,17 +27,40 @@ function App() {
     </div>
   );
 }
-const PlayerStyle = {
-  border: '2px solid green',
-  width: '400px',
-  margin: '10px',
-  borderRadius: '20px',
-  backgroundColor: 'tomato',
-  boxShadow: '10px 10px orange'
+
+function Products(props) {
+  const productStyle = {
+    border: '1px solid gray',
+    borderRadius: '5px',
+    backgroundColor: 'lightgray',
+    height: '200px',
+    width: '200px',
+    float: 'left',
+    color: 'purple'
+  }
+  {/** Destructure */}
+  const {name, price} = props.product;
+  
+  return (
+    <div style={productStyle}>
+      <h3>{name} </h3>
+      <h5>{price}</h5>
+      <button>Buy Now</button>
+    </div>
+
+  );
 }
 
-function Player(props){
-  return(
+function Player(props) {
+  const PlayerStyle = {
+    border: '2px solid green',
+    width: '400px',
+    margin: '10px',
+    borderRadius: '20px',
+    backgroundColor: 'tomato',
+    boxShadow: '10px 10px orange'
+  }
+  return (
     <div style={PlayerStyle}>
       <h3>Player Name: {props.name} </h3>
       <p>Role: {props.role} </p>

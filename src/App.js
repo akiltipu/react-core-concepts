5842d061cbe82e72bd3b60a59eb935818e9bf7f5
akiltipu <akilmahmod.tipu@gmail.com>
2020-03-11 03:52:27 +0600

    Learned Basic props

--- src/App.js
+++ src/App.js
@@ -3,30 +3,35 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const players = ['Shakib', 'Mustafiz', 'Mashrafe', 'Muminul'];
   return (
     <div className="App">
       <header className="App-header">
-        <p>React header </p>
-        <Person></Person>
-        <Person></Person>
-        <Person></Person>
-        <Person></Person>
-        
+        <h1>This is React header</h1>
+        <Player name={players[0]} role="All-rounder"></Player>
+        <Player name={players[1]} role="Bowler"></Player>
+        <Player name={players[2]} role="Captain"></Player>
+        <Player name={players[3]} role="Batsman"></Player>
       </header>
     </div>
   );
 }
-function Person(){
-  const personStyle = {
-    border: '2px solid red',
-    margin: '10px'
-  }
+const PlayerStyle = {
+  border: '2px solid green',
+  width: '400px',
+  margin: '10px',
+  borderRadius: '20px',
+  backgroundColor: 'tomato',
+  boxShadow: '10px 10px orange'
+}
+
+function Player(props){
   return(
-  <div style={personStyle}>
-    <h1>Name: Sakib Al Hasan</h1>
-    <h3>Hero of the year</h3>
-  </div>
-  );
+    <div style={PlayerStyle}>
+      <h3>Player Name: {props.name} </h3>
+      <p>Role: {props.role} </p>
+    </div>
+  )
 }
 
 export default App;
